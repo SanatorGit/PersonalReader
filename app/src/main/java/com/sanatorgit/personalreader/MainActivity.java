b3f816aa96cb8210d46b3e8872b03ae4aa83b551
Aleksey <aleksey.borovikov.00@mail.ru>
2019-09-10 09:08:54 +0300

    start work

---
+++ app/src/main/java/com/sanatorgit/personalreader/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sanatorgit.personalreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
